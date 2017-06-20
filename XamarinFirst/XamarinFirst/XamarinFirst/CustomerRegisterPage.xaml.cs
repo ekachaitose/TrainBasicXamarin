ac51febab4286a230e35b965936c4fbc7e811e9e
ekachai <ekachaitose11@gmail.com>
2017-06-20 16:06:31 +0700

    insert db

--- XamarinFirst/XamarinFirst/XamarinFirst/CustomerRegisterPage.xaml.cs
+++ XamarinFirst/XamarinFirst/XamarinFirst/CustomerRegisterPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinFirst.Models;
 
 namespace XamarinFirst
 {
@@ -15,15 +16,53 @@ namespace XamarinFirst
         public CustomerRegisterPage()
         {
             InitializeComponent();
+
+            childrenStepper.ValueChanged += ChildrenStepper_ValueChanged;
+            salarySlider.ValueChanged += SalarySlider_ValueChanged;
+            saveButton.Clicked += SaveButton_Clicked;
+
             var genders = new List<string>();
             genders.Add("Male");
             genders.Add("Female");
-            pickerGender.ItemsSource = genders;
-
-
-           
+            genderPicker.ItemsSource = genders;
         }
 
-    
+      
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            var customer = new Customer
+            {
+                Name = nameEntry.Text,
+                Surname = surnameEntry.Text,
+                Birthdate = birthdatePicker.Date,
+                Email = emailEntry.Text,
+                Telephone = telephoneEntry.Text,
+                IsMarry = marrySwitch.IsToggled,
+                Salary = (int)salarySlider.Value,
+                Children = (int)childrenStepper.Value,
+                Gender = genderPicker.SelectedItem.ToString(),
+                Photo = "",
+                Address = addressEditor.Text,
+                Website = websiteEntry.Text,
+                Username = usernameEntry.Text,
+                Password = passwordEntry.Text
+
+            };
+
+            var affected = Helpers.DbHelper.Current.CustomerAdd(customer);
+            if (affected > 0) DisplayAlert("Complete", "Register Success", "OK");
+           else DisplayAlert("Warning", "Something wrong", "OK");
+        }
+
+        void ChildrenStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            childrenLabel.Text = $"Children: {e.NewValue}";
+        }
+
+        void SalarySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            salaryLabel.Text = string.Format("Salary: {0:N0} Baht", e.NewValue);
+        }
     }
 }
