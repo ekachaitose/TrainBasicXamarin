02964bff5a0c1d60c5ae334aa177c6fa63644cae
ekachai <ekachaitose11@gmail.com>
2017-06-20 09:36:45 +0700

    first commit

---
+++ XamarinFirst/XamarinFirst/XamarinFirst/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinFirst
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var tp = new TabbedPage();
+            tp.Children.Add(new TabPage1());
+            tp.Children.Add(new TabPage2());
+            tp.Children.Add(new TabPage3());
+           
+
+            var cp = new CarouselPage();
+            cp.Children.Add(new TabPage1());
+            cp.Children.Add(new TabPage2());
+            cp.Children.Add(new TabPage3());
+
+            var np = new NavigationPage(new NavPage1());
+            MainPage = np;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamarinFirst/XamarinFirst/XamarinFirst/NavPage2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFirst
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavPage2 : ContentPage
+    {
+        public NavPage2()
+        {
+            InitializeComponent();
+            nextButton.Clicked += NextButton_Clicked;
+        }
+
+        private void NextButton_Clicked(object sender, EventArgs e)
+        {
+            nextButton.Navigation.PushAsync(new NavPage3());
+        }
+    }
+}
